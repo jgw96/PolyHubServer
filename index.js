28e6edc18a3021544295e3babd2ea5807fc4bd58
jgw96 <jgw9617@gmail.com>
2017-04-10 22:15:10 -0500

    issues notification

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const FCM = require('fcm-node');
 const serverKey = require('./key.json');
+const GitHubApi = require("github");
+
+const github = new GitHubApi({
+	protocol: "https",
+	host: "api.github.com", // should be api.github.com for GitHub
+	headers: {
+		"user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+	},
+	timeout: 5000
+});
 
 const fcm = new FCM(serverKey);
 
@@ -18,8 +28,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/token', (req, res) => {
-	console.log(req.body);
-	tokens.push(req.body.token);
 
 	const message = {
 		to: req.body.token,
@@ -39,6 +47,41 @@ app.post('/token', (req, res) => {
 		}
 	});
 
+});
+
+app.post('/issues', (req, res) => {
+	console.log(req.body.token);
+
+	const job = schedule.scheduleJob({ hour: 14, minute: 30 }, () => {
+		github.issues.getForRepo({
+			owner: req.body.owner,
+			repo: req.body.repo
+		}, function (err, res) {
+			if (err) {
+				console.log(err.toJSON());
+			} else {
+				console.log(res.data.length);
+			}
+		});
+
+		const message = {
+			to: req.body.token,
+
+			notification: {
+				title: 'Issues',
+				body: `${res.data.length} issues on ${req.body.repo}`,
+				icon: './images/512.png'
+			}
+		}
+
+		fcm.send(message, (err, response) => {
+			if (err) {
+				console.log("Something has gone wrong!")
+			} else {
+				console.log("Successfully sent with response: ", response)
+			}
+		});
+	});
 });
 
 app.listen(8080, () => {
